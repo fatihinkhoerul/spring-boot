0fedc437d1a47c92215536c899a9be74eb973abc
Khoerul Fatihin <khoerulfatihin1011@gmail.com>
2021-05-07 15:52:53 +0700

    First Commit

---
+++ src/main/java/id/fatih/springbootreact/controller/BiodataController.java
@@ -0,0 +1,19 @@
+package id.fatih.springbootreact.controller;
+
+import id.fatih.springbootreact.model.DefaultResponse;
+import id.fatih.springbootreact.model.dto.BiodataDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/biodata")
+public class BiodataController {
+    @GetMapping("/get")
+    public DefaultResponse<BiodataDto> getBiodata (){
+        BiodataDto biodataDto = new BiodataDto();
+        biodataDto.setNama("fatih");
+        biodataDto.setAlamat("bandung");
+        return DefaultResponse.ok(biodataDto);
+    }
+}
---
+++ src/main/java/id/fatih/springbootreact/model/dto/BiodataDto.java
@@ -0,0 +1,9 @@
+package id.fatih.springbootreact.model.dto;
+
+import lombok.Data;
+
+@Data
+public class BiodataDto {
+    private String nama;
+    private String alamat;
+}
