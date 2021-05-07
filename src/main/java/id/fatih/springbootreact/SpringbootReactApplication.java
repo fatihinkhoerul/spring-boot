fce0581b47d7a4920ce66061e36ba7362794dd81
Khoerul Fatihin <khoerulfatihin1011@gmail.com>
2021-05-07 15:27:12 +0700

    First commit

---
+++ src/main/java/id/fatih/springbootreact/SpringbootReactApplication.java
@@ -0,0 +1,13 @@
+package id.fatih.springbootreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootReactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/fatih/springbootreact/SpringbootReactApplicationTests.java
@@ -0,0 +1,13 @@
+package id.fatih.springbootreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
